b115b8f993c98df52d5eab1abd318614311ccf1c
pa-jovet <pa.jovet1@gmail.com>
2024-10-06 17:21:18 +0200

    Création du script des applis du matin

---
+++ matin.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Lancement des applications du matin
+# Je n'ai ni keepass ni virtualbox sur ma VM, j ai donc lancé des applis
+# présentes ici
+firefox
+nautilus /home/wilder/Documents/git
+gnome-calculator
